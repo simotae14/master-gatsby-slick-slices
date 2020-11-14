91c491f770e6845b176da5c5d169597f26ee8c76
Simone <startae14@gmail.com>
2020-11-14 23:50:16 +0100

    configure plugin to have the sanity data in gatsby

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,7 +1,26 @@
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: '.env',
+});
+
 export default {
   siteMetadata: {
     title: `Slick slices`,
     siteUrl: 'https://gatsby.pizza',
     description: 'The best pizza place in Milan!',
   },
-}
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // this is the name of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'qrz5awtv',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      }
+    },
+  ]
+};
